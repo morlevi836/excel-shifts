94fafa41cde4ff0fda10e197f412431658d312f7
Mor Levi <79837364+morlevi836@users.noreply.github.com>
2025-06-24 07:02:47 +0300

    feat: change vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// Needed to simulate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
